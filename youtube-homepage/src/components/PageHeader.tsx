fddf127c71afbc2d64e38e0f40474026e25f518e
Ernest <epospiky@gmail.com>
2024-02-17 22:43:17 +0000

    setup envn and install relevant files

---
+++ youtube-homepage/src/components/PageHeader.tsx
@@ -0,0 +1,21 @@
+import logo from "../assets/react.svg";
+import { Menu } from "lucide-react";
+
+function PageHeader() {
+  return (
+    <div className="flex gap-10 lg:gap-20 justify-between">
+      <div className="flex gap-4 items-center flex-shrink-0">
+        <button>
+          <Menu />
+        </button>
+        <a href="/">
+          <img src={logo} className="h-6" />
+        </a>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default PageHeader;
