8b74053f43de9fe43863aedaa93ef14c1a394cd4
Ernest <epospiky@gmail.com>
2024-02-18 22:49:36 +0000

    added stylings for buttons

---
+++ youtube-homepage/src/components/Button.tsx
@@ -0,0 +1,30 @@
+import { VariantProps, cva } from "class-variance-authority";
+
+const buttonStyles = cva(["hover:bg-secondary-hover", "transition-colors"], {
+  variants: {
+    variant: {
+      default: ["bg-secondary", "hover:bg-secondary-hover"],
+      ghost: ["hover:bg-gray-100"],
+    },
+    size: {
+      default: ["rounded", "p-2"],
+      icon: [
+        "rounded-full",
+        "w-10",
+        "h-10",
+        "flex",
+        "items-center",
+        "justify-center",
+        "p-2.5",
+      ],
+    },
+  },
+});
+
+const classes = buttonStyles({ size: "icon" });
+type ButtonProps = VariantProps<typeof buttonStyles>;
+function Button({ variant, size }: ButtonProps) {
+  return <button className={buttonStyles({ variant, size })} />;
+}
+
+export default Button;
